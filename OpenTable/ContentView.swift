00f1f0781cb6e9f63015210105b5d92f0c3f862f
Ngo Quoc Dat <datlechin@gmail.com>
2025-12-16 18:52:33 +0700

    Init project

---
+++ OpenTable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenTable
+//
+//  Created by Ngo Quoc Dat on 16/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenTable/OpenTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTableApp.swift
+//  OpenTable
+//
+//  Created by Ngo Quoc Dat on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
